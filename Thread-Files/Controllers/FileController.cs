165232b0ffbb1d3dd5700cf211e82edc31a18c26
unknown <asgarkhanmb@code.edu.az>
2024-03-27 23:48:59 +0400

    Thread-Files-Practice

---
+++ Thread-Files/Controllers/FileController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thread_Files.Extensions;
+using Thread_Files.Services;
+using Thread_Files.Services.Interfaces;
+
+namespace Thread_Files.Controllers
+{
+    internal class FileController
+    {
+        private readonly IFileService _fileService;
+
+        public FileController()
+        {
+            _fileService=new FileService();
+        }
+        public async Task ReadDataAsync()
+        {
+            string result = await _fileService.ReadFromFileAsync("C:\\Users\\Əsgərxan\\Desktop\\Thread-Files\\file6.txt");
+            Console.WriteLine(result);
+        }
+
+
+       // "C:\\Users\\Əsgərxan\\Desktop\\Thread-Files"
+        public async Task CreatedFileWithContentAsync()
+        {
+            await Console.Out.WriteLineAsync("Add path:");
+            string path =Console.ReadLine();
+
+            await Console.Out.WriteLineAsync("Add file name:");
+            string fileName = Console.ReadLine();
+
+            await Console.Out.WriteLineAsync("Add your text:");
+
+            string text=Console.ReadLine();
+
+            string resultPath = path.GenerateFullPath(fileName);
+            await _fileService.WriteToNewFileAsync(resultPath,text);
+        }
+        public async Task DeleteAsync()
+        {
+            await _fileService.DeleteAsync("C:\\Users\\Əsgərxan\\Desktop\\Thread-Files\\file7.txt");
+        }
+    }
+}
---
+++ Thread-Files/Program.cs
@@ -0,0 +1,62 @@
+﻿//void Method1()
+//{
+//    //Thread.Sleep(3000);
+//    for (int i = 0; i < 100; i++)
+//    {
+//        Console.WriteLine("Method1 - " + i);
+//    }
+//}
+
+//void Method2()
+//{
+//    for (int i = 0; i < 100; i++)
+//    {
+//        Console.WriteLine("Method2 - " + i);
+//    }
+//}
+//void Method3()
+//{
+//    for (int i = 0; i < 100; i++)
+//    {
+//        Console.WriteLine("Method3 - " + i);
+//    }
+//}
+
+
+
+
+//Thread thread1 = new Thread(Method1);
+//Thread thread2 = new Thread(Method2);
+
+//thread1.Start();
+
+//Method3();
+
+//thread2.Start();
+
+
+
+
+//file.WriteToExistFile("C:\\Users\\Əsgərxan\\Desktop\\Thread-Files\\file2.txt", "Salam Xacker bey");
+//file.WriteToExistFile("C:\\Users\\Əsgərxan\\Desktop\\Thread-Files\\file3.txt", "Salam Behruz bey");
+
+//file.ReadFromFile("C:\\Users\\Əsgərxan\\Desktop\\Thread-Files\\file2.txt");
+
+//file.WriteToNewFile("C:\\Users\\Əsgərxan\\Desktop\\Thread-Files\\file6.txt", "Necesiz Nesir bey ?");
+//file.WriteToNewFile("C:\\Users\\Əsgərxan\\Desktop\\Thread-Files\\file7.txt", "Necesiz Nesir bey ?");
+
+
+//using Thread_Files.Controllers;
+
+//FileController fileController = new FileController();
+
+//await fileController.ReadDataAsync();
+
+//await fileController.CreatedFileWithContentAsync();
+
+//await fileController.DeleteAsync();
+
+
+
+
+
